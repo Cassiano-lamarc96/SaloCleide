2ca62bcc2e902759783fd921bdf65efb6dfaa8ef
Cassiano Lamarc <cacassiano2011@hotmail.com>
2018-11-23 10:54:36 -0300

    Tela de cadastro e consulta de serviço

---
+++ app/src/main/java/com/example/sigfas/salocleide/actConsultaServicos.java
@@ -0,0 +1,13 @@
+package com.example.sigfas.salocleide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class actConsultaServicos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_act_consulta_servicos);
+    }
+}
