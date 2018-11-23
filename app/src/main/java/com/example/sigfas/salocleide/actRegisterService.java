7529842a9dcc484eb655d34e20832eb5f60522ed
Cassiano Lamarc <cacassiano2011@hotmail.com>
2018-11-23 14:19:27 -0300

    eventos OK. falta conectar com sqLite

--- app/src/main/java/com/example/sigfas/salocleide/actRegisterService.java
+++ app/src/main/java/com/example/sigfas/salocleide/actRegisterService.java
@@ -1,7 +1,11 @@
 package com.example.sigfas.salocleide;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class actRegisterService extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class actRegisterService extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_register_service);
+    }
+
+    private boolean cadastrarServico(){
+        //Por enquanto retorna true, mas servirá para cadastrar o serviço.
+        return  true;
+    }
+
+    protected void cadastrar(View view){
+        Intent it = new Intent(this, actConsultaServicos.class);
+        DialogFragment dialog = new DialogFragment();
+        if (cadastrarServico()){
+            dialog.show(getSupportFragmentManager(), "Cadastro Realizado com sucesso!");
+            startActivity(it);
+        }else{
+            dialog.show(getSupportFragmentManager(), "Não foi possível cadastrar. Tente novamente!");
+        }
     }
 }
